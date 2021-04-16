59a010e35310f068d6d4380676e5afa6d143501a
luckyCatMiao <360382992@qq.com>
2021-04-16 19:09:30 +0800

    add simple noise shader Signed-off-by: luckyCatMiao <360382992@qq.com>

---
+++ script/SimpleNoise.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+[ExecuteInEditMode]
+public class SimpleNoise : MonoBehaviour
+{
+    public Shader shader;
+
+    private void OnRenderImage(RenderTexture src, RenderTexture dest)
+    {
+        Material material = new Material(shader);
+        Graphics.Blit(src, dest, material);
+    }
+}
